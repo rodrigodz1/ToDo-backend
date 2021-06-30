1b3e99a825d31a8358110b93ab4f94dda9e3f120
Rodrigo Brito <spensmusic@gmail.com>
2021-06-30 18:49:53 -0300

    db info

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-    //url: process.env.DEV_DATABASE_URL,
-    username: "rodrigo",
-    password: "test",
-    database: "trabalho",
-    host: "pgsql-seg",
+    //url: process.env.DATABASE_URL,
+    username: "yrigdewgextetg",
+    password: "892d210d1aaeb0d1e8e3938f1df1bf6b085ae31d2c1ac0eb219d390d86b30718",
+    database: "ddov1flig3fl9l",
+    host: "ec2-52-86-25-51.compute-1.amazonaws.com",
     dialect: "postgres",
     define: {
         timestamps: true,
